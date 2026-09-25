717905103b45c243542ac7506cd1531dbe68c516
atsumuru <phillip8@my.erau.edu>
2026-09-25 15:05:21 -0700

    Add UI components.

---
+++ BattleRoyale/Assets/Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using System.Linq;
+
+public class GameManager : MonoBehaviourPun
+{
+    [Header("Players")]
+    public string playerPrefabLocation;
+    public PlayerController[] players;
+    public Transform[] spawnPoints;
+    public int alivePlayers;
+
+    private int playersInGame;
+
+    // instance
+    public static GameManager instance;
+
+    void Awake ()
+    {
+        instance = this;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        players = new PlayerController[PhotonNetwork.PlayerList.Length];
+        alivePlayers = players.Length;
+        photonView.RPC("ImInGame", RpcTarget.AllBuffered);
+    }
+
+    [PunRPC]
+    void ImInGame ()
+    {
+        playersInGame++;
+        if(PhotonNetwork.IsMasterClient && playersInGame == PhotonNetwork.PlayerList.Length)
+        photonView.RPC("SpawnPlayer", RpcTarget.All);
+    }
+
+    [PunRPC]
+    void SpawnPlayer ()
+    {
+        GameObject playerObj = PhotonNetwork.Instantiate(playerPrefabLocation, spawnPoints[Random.Range(0, spawnPoints.Length)].position, Quaternion.identity);
+        // initialize the player for all other players
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
